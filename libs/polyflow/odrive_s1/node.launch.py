4c5395f9e1d7be5f57b062d81675312e4e917010
Drew Swinney <drewswinney@gmail.com>
2025-12-17 21:16:07 -0500

    Deploy code for PA1

---
+++ libs/polyflow/odrive_s1/node.launch.py
@@ -0,0 +1,26 @@
+import json
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    parameters = json.loads('{"joint":null,"control_mode":"position","transport":"can","units":"radians","can.node_id":0,"can.interface":"socketcan","can.channel":"can0","can.bitrate":1000000,"can.poll_hz":50,"can.request_iq":false,"can.heartbeat_timeout_s":2,"can.enable_closed_loop_on_start":true,"can.torque_constant":0,"limit.lower_position":null,"limit.upper_position":null,"limit.position_step":null,"limit.max_effort":null,"limit.effort_step":null,"limit.max_velocity":null,"limit.velocity_step":null}')
+    configuration = json.loads('{"namespace":"/robot/base","rate_hz":150,"lifecycle":true}')
+    inbound_connections = json.loads('[]')
+    outbound_connections = json.loads('[]')
+    env = {
+        "POLYFLOW_NODE_ID": "6943622059fabdb5a0647574",
+        "POLYFLOW_PARAMETERS": json.dumps(parameters),
+        "POLYFLOW_CONFIGURATION": json.dumps(configuration),
+        "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(inbound_connections),
+        "POLYFLOW_OUTBOUND_CONNECTIONS": json.dumps(outbound_connections),
+    }
+
+    return LaunchDescription([
+        Node(
+            package="odrive_s1",
+            executable="odrive_s1_node",
+            name="odrive_s1_node",
+            output="screen",
+            additional_env=env
+        )
+    ])
